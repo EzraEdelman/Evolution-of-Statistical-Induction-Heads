7053c0868169b0e68c95f74d4b8eeaac47471a46
Ezra Edelman <ez.edelman@gmail.com>
2024-11-14 11:58:36 -0800

    testing optimizations

--- datasets.py
+++ datasets.py
@@ -29,10 +29,10 @@ class ngrams(Dataset):
         self.last_token_only = last_token_only
         self.device = device
         self.n = n - 1
-        self.transition_matrix_gen = torch.distributions.dirichlet.Dirichlet(torch.ones((num_symbols**(n-1),num_symbols), device = device)).sample
+        self.transition_matrix_gen = torch.distributions.dirichlet.Dirichlet(torch.ones((num_symbols**(n-1),num_symbols))).sample
         
         # Compute powers of num_symbols
-        self.powers = self.num_symbols ** torch.arange(self.n - 1, -1, -1, device=device, dtype=torch.long)  # Shape: (n,)
+        self.powers = self.num_symbols ** torch.arange(self.n - 1, -1, -1, dtype=torch.long)  # Shape: (n,)
 
         self.conv = torch.tensor([num_symbols ** k for k in range(self.n)])
 
